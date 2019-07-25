b53561e5bac802393268dfdac927e297e78fb2db
Mike Solomon <msolo@gmail.com>
2019-07-24 23:43:39 -0700

    Remove pyzy since it now has it's own repo.

--- pyzy/test/benchmark.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "exec python"
-time for ((x=0;x<100;x=x+1)) do python ./test_small_cpu_task.py > /dev/null 2>&1 ; done
-
-echo "exec pyzy_client"
-time for ((x=0;x<100;x=x+1)) do ../pyzy_client ./test_small_cpu_task.py > /dev/null 2>&1 ; done
